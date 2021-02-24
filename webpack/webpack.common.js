f2f32d2dbf6aadccc020405476c55f3de80cdd8f
Sergio Ostanioni <ostanioni@gmail.com>
2021-02-24 10:55:42 +0700

    v 0.5.1

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,23 +1,8 @@
 /* eslint-disable */
 /* tslint:disabled */
 
-/***___BABEL_LOADER___ ***/
-const BABEL = {
-  test: /\.js$/,
-  exclude: /node_modules/,
-  loader: "babel-loader",
-}
-/***___TSX_LOADER___***/
-
-const TSX = {
-  test: /\.tsx?$/,
-  loader: 'ts-loader',
-  exclude: /node_modules/,
-}
-
 const path = require('path')
 const webpack = require('webpack')
-
 
 /*_____________CONTEXT_______________ */
 const CONTEXT = path.resolve(__dirname, '../')
@@ -28,36 +13,36 @@ const $ENTRY = './src/index.ts'
 module.exports = {
   context: CONTEXT,
   entry: {
-   app: $ENTRY
+    app: $ENTRY
   },
   output: {
     filename: '[contenthash].js',
     path: `${CONTEXT}/dist`,
-    publicPath: ASSET_PATH,
+    publicPath: ASSET_PATH
   },
   module: {
-    rules: [ TSX, BABEL ]
+    rules: []
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: ['.tsx', '.ts', '.js'],
     alias: {
-     'react-dom': '@hot-loader/react-dom',
-      pages:      `${CONTEXT}/src/pages`,
-      layouts:    `${CONTEXT}/src/layouts`,
+      'react-dom': '@hot-loader/react-dom',
+      pages: `${CONTEXT}/src/pages`,
+      layouts: `${CONTEXT}/src/layouts`,
       components: `${CONTEXT}/src/components`,
-      resources:  `${CONTEXT}/src/resources`,
-      tables:     `${CONTEXT}/src/tables`,
-      stores:     `${CONTEXT}/src/stores`,
-      styled:     `${CONTEXT}/src/styled`,
-      ts:         `${CONTEXT}/src/typescript`,
-      algs:       `${CONTEXT}/src/typescript/algorithms`,
-      webgl:      `${CONTEXT}/src/webgl`,
-      polyfills:  `${CONTEXT}/src/polyfills`,
-      resources:  `${CONTEXT}/public/resources`,
-      workers:    `${CONTEXT}/public/workers`,
-      css:        `${CONTEXT}/public/css`,
-      imgs:       `${CONTEXT}/public/imgs`,
-      themes:     `${CONTEXT}/src/themes`,
-    },
-  },
+      resources: `${CONTEXT}/src/resources`,
+      tables: `${CONTEXT}/src/tables`,
+      store: `${CONTEXT}/src/stores`,
+      styled: `${CONTEXT}/src/styled`,
+      ts: `${CONTEXT}/src/typescript`,
+      algs: `${CONTEXT}/src/typescript/algorithms`,
+      webgl: `${CONTEXT}/src/webgl`,
+      polyfills: `${CONTEXT}/src/polyfills`,
+      resources: `${CONTEXT}/public/resources`,
+      workers: `${CONTEXT}/public/workers`,
+      css: `${CONTEXT}/public/css`,
+      imgs: `${CONTEXT}/public/imgs`,
+      themes: `${CONTEXT}/src/themes`
+    }
+  }
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,11 +2,12 @@
 /* tslint:disabled */
 
 /***_____IMPORTS______***/
-const path    = require('path')
+const path = require('path')
 const webpack = require('webpack')
-const { merge }   = require('webpack-merge')
-const common  = require('./webpack.common.js')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 console.clear()
 console.log('dev-mode starting ...')
@@ -15,11 +16,52 @@ console.log('dev-mode starting ...')
 const CONTEXT = path.resolve(__dirname, '../')
 const SRC = `${CONTEXT}/src`
 
-/***___SOURCE_MAP____***/
-const JS_SOURCE_MAP = {
-  test: /\.js$/,
-  enforce: 'pre',  
-  use: [ 'source-map-loader' ],
+/***___BABEL_LOADER___ ***/
+const BABEL = {
+  test: /\.m?js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader'
+  }
+}
+/***___TSX_LOADER___***/
+const TSX = {
+  test: /\.tsx?$/,
+  loader: 'ts-loader',
+  exclude: /node_modules/,
+  options: {
+    // disable type checker - we will use it in fork plugin
+    transpileOnly: true
+  }
+}
+/*________PLUGIN_OPTIONS_____________*/
+const htmlPluginOptions = {
+  inject: true,
+  template: `${CONTEXT}/public/index.html`,
+  minify: {
+    removeComments: false,
+    collapseWhitespace: false,
+    removeRedundantAttributes: true,
+    removeEmptyAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    removeScriptTypeAttributes: false,
+    useShortDoctype: true,
+    keepClosingSlash: true,
+    minifyJS: false,
+    minifyCSS: false,
+    minifyURLs: false
+  }
+}
+const forkTsPluginOptions = {
+  eslint: {
+    files: './src/**/*.{ts,tsx,js,jsx}' // required - same as command `eslint ./src/**/*.{ts,tsx,js,jsx} --ext .ts,.tsx,.js,.jsx`
+  },
+  typescript: {
+    diagnosticOptions: {
+      semantic: true,
+      syntactic: true
+    }
+  }
 }
 
 module.exports = merge(common, {
@@ -33,28 +75,14 @@ module.exports = merge(common, {
   },
   devtool: 'cheap-module-source-map', // 'source-map',
   module: {
-    rules: [ JS_SOURCE_MAP ]
+    rules: [TSX, BABEL]
   },
   optimization: {
     minimize: false,
     usedExports: true
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: `${CONTEXT}/public/index.html`,
-      minify: {
-        removeComments: false,
-        collapseWhitespace: false,
-        removeRedundantAttributes: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        removeScriptTypeAttributes: false,
-        useShortDoctype: true,        
-        keepClosingSlash: true,
-        minifyJS: false,
-        minifyCSS: false,
-        minifyURLs: false,
-      }
-    })],
-});
+    new HtmlWebpackPlugin(htmlPluginOptions),
+    new ForkTsCheckerWebpackPlugin(forkTsPluginOptions)
+  ]
+})
