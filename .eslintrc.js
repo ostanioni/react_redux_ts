2e8e8e5e9ffc547b49cd89b123f974d990f46669
Sergio Ostanioni <ostanioni@gmail.com>
2021-02-24 10:54:09 +0700

    rules added

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,14 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module'
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+      parserOptions: {
+        project: ['./tsconfig.json'] // Specify it only for TypeScript files
+      }
+    }
+  ],
   plugins: ['@typescript-eslint', 'react-redux', 'prettier'],
   extends: [
     'eslint:recommended',
@@ -31,9 +39,30 @@ module.exports = {
   rules: {
     indent: ['warn', 2],
     'linebreak-style': ['warn', 'unix'],
-    quotes: ['warn', 'double'],
-    // semi: 'none',
-    '@typescript-eslint/semicolon': 'warn',
-    '@typescript-eslint/trailing-comma': false
+    'comma-dangle': [
+      'warn',
+      {
+        arrays: 'always',
+        objects: 'always',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never'
+      }
+    ],
+    '@typescript-eslint/comma-dangle': ['warn'],
+
+    quotes: ['warn', 'single', { allowTemplateLiterals: true }],
+    '@typescript-eslint/quotes': ['warn'],
+
+    semi: ['error', 'never', { omitLastInOneLineBlock: true }],
+    '@typescript-eslint/semicolon': ['warn'],
+
+    'comma-spacing': ['error', { before: false, after: true }],
+    '@typescript-eslint/comma-spacing': ['warn'],
+
+    'brace-style': ['warn', '1tbs', { allowSingleLine: true }],
+    '@typescript-eslint/brace-style': ['warn'],
+
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
   }
 }
